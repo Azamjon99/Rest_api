a4fe08bcaf7400f65085593d3e83d4a1759a016b
Azamjon99 <oktamovazam1@gmail.com>
2020-11-16 15:08:20 +0500

    Working on Api resources

--- app/Http/Resources/Product/ProductRes.php
+++ app/Http/Resources/Product/ProductRes.php
@@ -18,8 +18,14 @@ class ProductRes extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'stock'=>$this->stock ==0 ? 'out of stock' : $this->stock,
             'discount'=>$this->discount,
+            'totalPrice'=>round((1-$this->discount/100)*$this->price, 1),
+            'rating'=>$this->reviews->count()>0 ? 
+            round($this->reviews->sum('star')/$this->reviews->count(), 2) : "No ratings yet",
+            'href'=>[
+                'reviews'=>route('reviews.index', $this->id)
+            ]
                 ];   
     }
 }
